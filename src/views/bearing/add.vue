6120d788682163f81ddadafbd1ec51785c9da5ab
zyl_1.3_dev <http://zhaoyueling@172.16.139.95/root/CloudSuite_web.git>
2021-08-22 22:15:51 +0800

    '1'

--- src/views/bearing/add.vue
+++ src/views/bearing/add.vue
@@ -49,6 +49,17 @@
                 <el-form-item label="描述内容">
                   <el-input type="textarea" v-model="form.content"></el-input></el-form-item>
               </el-col>
+              <el-col :span="24">
+                <el-upload
+                  class="upload-demo"
+                  drag
+                  action="http://war3.xxroom.xyz/z/qiniu/uploadImg"
+                  multiple>
+                  <i class="el-icon-upload"></i>
+                  <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+                  <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+                </el-upload>
+              </el-col>
             </el-row>
         </div>
         <div class="el-footer">
